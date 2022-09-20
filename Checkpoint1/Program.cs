d6a2f9a57baa9edc929066ad913cf91c47cb1694
BR0M1CH <110671447+BR0M1CH@users.noreply.github.com>
2022-09-20 21:48:20 +0300

    GB1

---
+++ Checkpoint1/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+    Console.WriteLine("Введите количество элементов");
+    int n =int.Parse(Console.ReadLine());
+    string[] ArrString;
+    ArrString = new string[n];
+    Console.WriteLine("Введите элементы");
+    for (int i = 0; i < n; i++)
+        {
+        ArrString[i] = (Console.ReadLine());
+        }
+    Console.WriteLine("Массив имеет следующий вид:");
+    for (int i = 0; i < n; i++)
+
+    {
+        Console.Write($"{ArrString[i]}\t");
+    }
+    Console.WriteLine("\nПреобразованный массив имеет следующий вид:");
+    for (int i = 0; i < n; i++)
+    {
+        if (ArrString[i].Length <= 3)
+        {
+            Console.Write($"{ArrString[i]}\t");
+        }
+        else 
+        {
+            continue;
+        } 
+    }
